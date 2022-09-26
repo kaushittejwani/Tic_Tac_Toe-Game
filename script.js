d65b6ec29f847ee768c51feea157963663c4e3cd
kaushittejwani <95282960+kaushittejwani@users.noreply.github.com>
2022-09-26 23:52:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let turn = "X";
 let isgameover = false;
 let scoreX = 0;
 let scoreY = 0;
+let Ele_Player=document.getElementsByClassName('boxtext').innerText;
 
 // Function to change the turn
 const changeTurn = () => {
@@ -47,26 +48,32 @@ const checkWin = () => {
       document.getElementsByClassName("box").style = `background-color=blue`;
 
       document.querySelector(".line").style.width = "26vw";
-      document.getElementsByClassName("container").style.backgroundColor =
-        value;
-      confirm("congrats u win");
-      if (confirm) {
-        let boxtexts = document.querySelectorAll(".boxtext");
-        Array.from(boxtexts).forEach((element) => {
-          element.innerText = "";
-        });
-        turn = "X";
-        isgameover = false;
-        document.querySelector(".line").style.width = "0vw";
-        document.getElementsByClassName("info")[0].innerText =
-          "Turn for " + turn;
-        document
-          .querySelector(".imgbox")
-          .getElementsByTagName("img")[0].style.width = "0px";
-      }
-    }
+     
+
+          if( boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+            boxtext[e[0]].innerText !== ""){
+              setTimeout(() => {
+                                            
+                window.location.reload();
+
+              }, 2000);
+            }
+            if(boxtext[e[0].innerText]===boxText[e[1]].innerText &&  boxtext[e[2]].innerText !== boxtext[e[1]].innerText &&
+              boxtext[e[0]].innerText !== ""){
+                document.querySelector('.info').style.innerText="Game is over";
+              }
+            }
+            
+           
+            
+          
+        
+    
   });
 };
+
+
 
 // Game Logic
 // music.play()
@@ -79,18 +86,35 @@ Array.from(boxes).forEach((element) => {
     if (!isgameover) {
       if (boxtext.innerText === "") {
         boxtext.innerText = turn;
+        
         turn = changeTurn();
 
         audioTurn.play();
+
         checkWin();
         if (!isgameover) {
           document.getElementsByClassName("info")[0].innerText =
             "Turn for " + turn;
         }
+        if (turn == "X") {
+      boxtext.style.color = "red";
+      Ele_Player.style.color = "red";
+    }
+    
+    else {
+      boxtext.style.color = "purple";
+      Ele_Player.style.color = "blue";
+
+    }
+        
       }
     }
+   
+    
+   
   });
 });
+
 
 // Add onclick listener to reset button
 
@@ -110,5 +134,3 @@ const myfunc = () => {
   var element = document.getElementsByClassName("box");
   element.classList.toggle("my-style");
 };
-
- 
